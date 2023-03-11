f3a7ca06da2add3445505e117ccf84f7708f7eee
Jakub Slabý <jakubslaby09@gmail.com>
2023-03-11 14:06:17 +0100

    don't use `HeaderMap` when making a request

--- src/requests.rs
+++ src/requests.rs
@@ -3,38 +3,12 @@ use serde_json::{json, Value, Map};
 
 use crate::{config::YoutubeConfig, BASE_URL};
 
-pub(crate) async fn create_api_request(_config: &YoutubeConfig, endpoint_name: &str, input_variables: Map<String, Value>) -> Result<Value, reqwest::Error> {
+pub(crate) async fn create_api_request(config: &YoutubeConfig, endpoint_name: &str, input_variables: Map<String, Value>) -> Result<Value, reqwest::Error> {
     let mut url = Url::parse(BASE_URL).expect("invalid base url");
-    url.set_path(format!("youtubei/{}/{}", _config.innertube_api_version, endpoint_name).as_str());
-    
-    let config = _config.clone();
-    let headers_map = Vec::from([
-        ("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0"),
-        ("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*//*;q=0.8"),
-        ("Accept-Language", "en-US,en;q=0.5"),
-        ("Upgrade-Insecure-Requests", "1"),
-        ("Sec-Fetch-Dest", "document"),
-        ("Sec-Fetch-Mode", "navigate"),
-        ("Sec-Fetch-Site", "none"),
-        ("Sec-Fetch-User", "?1"),
-        ("x-origin", BASE_URL),
-        ("X-Goog-Visitor-Id", &config.visitor_data),
-        ("X-YouTube-Client-Name", &config.innertube_context_client_name),
-        ("X-YouTube-Client-Version", &config.innertube_client_version),
-        ("X-YouTube-Device", &config.device),
-        ("X-YouTube-Page-CL", &config.page_cl),
-        ("X-YouTube-Page-Label", &config.page_build_label),
-        //"X-YouTube-Utc-Offset", String(-new Date().getTimezoneOffset()),
-        //"X-YouTube-Time-Zone", new Intl.DateTimeFormat().resolvedOptions().timeZone
-    ]);
-
-    let mut headers = HeaderMap::new();
-    for header in headers_map {
-        headers.insert(header.0, HeaderValue::from_str(header.1).unwrap());
-    }
+    url.set_path(format!("youtubei/{}/{}", config.innertube_api_version, endpoint_name).as_str());
 
     let mut body = Map::new();
-    for item in api_context(_config) {
+    for item in api_context(config) {
         body.insert(item.0, item.1);
     }
     for item in input_variables {
@@ -42,7 +16,21 @@ pub(crate) async fn create_api_request(_config: &YoutubeConfig, endpoint_name: &
     }
 
     let res = reqwest::Client::new().post(url)
-        .headers(headers)
+        .header("User-Agent", "Mozilla/5.0 (X11; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0")
+        .header("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*//*;q=0.8")
+        .header("Accept-Language", "en-US,en;q=0.5")
+        .header("Upgrade-Insecure-Requests", "1")
+        .header("Sec-Fetch-Dest", "document")
+        .header("Sec-Fetch-Mode", "navigate")
+        .header("Sec-Fetch-Site", "none")
+        .header("Sec-Fetch-User", "?1")
+        .header("x-origin", BASE_URL)
+        .header("X-Goog-Visitor-Id", &config.visitor_data)
+        .header("X-YouTube-Client-Name", config.innertube_context_client_name.to_string())
+        .header("X-YouTube-Client-Version", &config.innertube_client_version)
+        .header("X-YouTube-Device", &config.device)
+        .header("X-YouTube-Page-CL", config.page_cl.to_string())
+        .header("X-YouTube-Page-Label", &config.page_build_label)
         .body(serde_json::to_string(&body).unwrap())
         .send()
         .await?
