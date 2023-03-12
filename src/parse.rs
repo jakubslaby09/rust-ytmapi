a564adace9cae2a778154bb30fd624de2441d8b0
Jakub Slabý <jakubslaby09@gmail.com>
2023-03-12 09:48:47 +0100

    extract json pointers from `parse.rs` to a new submodule

--- src/parse.rs
+++ src/parse.rs
@@ -1,8 +1,12 @@
+mod pointers;
+
 use std::{fmt::Display, error::Error};
 
 use serde_json::Value;
 
 use crate::Client;
+
+use self::pointers::*;
 
 #[derive(Debug, Clone)]
 pub struct Artist {
@@ -17,44 +21,25 @@ pub struct Artist {
 impl Artist {
     pub(crate) fn parse(res: Value) -> Result<Self, ResponseParseError> {
         Ok(Artist {
-            name: string_from_json(&res, "/header/musicImmersiveHeaderRenderer/title/runs/0/text")?,
-            description: res.pointer("/header/musicImmersiveHeaderRenderer/description/runs/0/text").and_then(
+            name: string_from_json(&res, ARTIST_NAME)?,
+            description: res.pointer(ARTIST_DESCIPTION).and_then(
                 |it| Some(it.as_str()?.to_string())
             ),
-            albums: iter_from_json(&res, "/contents/singleColumnBrowseResultsRenderer/tabs/0/tabRenderer/content/sectionListRenderer/contents/1/musicCarouselShelfRenderer/contents")?.filter_map(|item| -> Option<Product> {
+            albums: iter_from_json(&res, ARTIST_ALBUMS)?.filter_map(|item| -> Option<Product> {
                 Some(Product {
-                    name: item.pointer("/musicTwoRowItemRenderer/title/runs/0/text")?.as_str()?.to_string(),
-                    browse_id: item.pointer("/musicTwoRowItemRenderer/title/runs/0/navigationEndpoint/browseEndpoint/browseId")?.as_str()?.to_string(),
-                    year: item.pointer("/musicTwoRowItemRenderer/subtitle/runs/2/text")?.as_str()?.to_string(),
+                    name: item.pointer(ARTIST_PRODUCT_NAME)?.as_str()?.to_string(),
+                    browse_id: item.pointer(ARTIST_PRODUCT_ID)?.as_str()?.to_string(),
+                    year: item.pointer(ARTIST_ALBUM_YEAR)?.as_str()?.to_string(),
                 })
             }).collect(),
-            singles: iter_from_json(&res, "/contents/singleColumnBrowseResultsRenderer/tabs/0/tabRenderer/content/sectionListRenderer/contents/2/musicCarouselShelfRenderer/contents")?.filter_map(|item| -> Option<Product> {
+            singles: iter_from_json(&res, ARTIST_SINGLES)?.filter_map(|item| -> Option<Product> {
                 Some(Product {
-                    name: item.pointer("/musicTwoRowItemRenderer/title/runs/0/text")?.as_str()?.to_string(),
-                    browse_id: item.pointer("/musicTwoRowItemRenderer/title/runs/0/navigationEndpoint/browseEndpoint/browseId")?.as_str()?.to_string(),
-                    year: item.pointer("/musicTwoRowItemRenderer/subtitle/runs/0/text")?.as_str()?.to_string(),
+                    name: item.pointer(ARTIST_PRODUCT_NAME)?.as_str()?.to_string(),
+                    browse_id: item.pointer(ARTIST_PRODUCT_ID)?.as_str()?.to_string(),
+                    year: item.pointer(ARTIST_SINGLE_YEAR)?.as_str()?.to_string(),
                 })
             }).collect(),
         })
-    }
-}
-
-fn value_from_json<'a>(value: &'a Value, pointer: &str) -> Result<&'a Value, ResponseParseError> {
-    value.pointer(pointer)
-    .ok_or(ResponseParseError::MissingValue(pointer.to_string()))
-}
-
-fn string_from_json(value: &Value, pointer: &str) -> Result<String, ResponseParseError>{
-    match value_from_json(value, pointer)?.as_str() {
-        Some(it) => Ok(it.to_string()),
-        None => Err(ResponseParseError::BadValue(pointer.to_string(), value.clone())),
-    }
-}
-
-fn iter_from_json<'a>(value: &'a Value, pointer: &str) -> Result<std::slice::Iter<'a, Value>, ResponseParseError> {
-    match value_from_json(value, pointer)?.as_array() {
-        Some(it) => Ok(it.iter()),
-        None => Err(ResponseParseError::BadValue(pointer.to_string(), value.clone())),
     }
 }
 
@@ -83,13 +68,13 @@ impl Album {
     pub(crate) fn parse(res: Value) -> Option<Self> {
         Some(
             Album {
-                name: res.pointer("/header/musicDetailHeaderRenderer/title/runs/0/text")?.as_str()?.to_string(),
-                year: res.pointer("/header/musicDetailHeaderRenderer/subtitle/runs/4/text")?.as_str()?.to_string(),
-                tracks: res.pointer("/contents/singleColumnBrowseResultsRenderer/tabs/0/tabRenderer/content/sectionListRenderer/contents/0/musicShelfRenderer/contents")?
+                name: res.pointer(ALBUM_NAME)?.as_str()?.to_string(),
+                year: res.pointer(ALBUM_YEAR)?.as_str()?.to_string(),
+                tracks: res.pointer(ALBUM_TRACKS)?
                 .as_array()?.into_iter().enumerate().filter_map(|(track_num, item)| -> Option<Track> {
                     Some(Track {
-                        name: item.pointer("/musicResponsiveListItemRenderer/flexColumns/0/musicResponsiveListItemFlexColumnRenderer/text/runs/0/text")?.as_str()?.to_string(),
-                        video_id: item.pointer("/musicResponsiveListItemRenderer/flexColumns/0/musicResponsiveListItemFlexColumnRenderer/text/runs/0/navigationEndpoint/watchEndpoint/videoId")?.as_str()?.to_string(),
+                        name: item.pointer(ALBUM_TRACK_NAME)?.as_str()?.to_string(),
+                        video_id: item.pointer(ALBUM_TRACK_ID)?.as_str()?.to_string(),
                         track_num: track_num + 1,
                     })
                 }).collect(),
@@ -115,12 +100,12 @@ pub struct ArtistSearchResult {
 impl ArtistSearchResult {
     pub(crate) fn parse(res: Value) -> Option<Vec<Self>> {
         Some(
-            res.pointer("/contents/tabbedSearchResultsRenderer/tabs/0/tabRenderer/content/sectionListRenderer/contents/1/musicShelfRenderer/contents")?
+            res.pointer(SEARCHED_ARTISTS)?
             .as_array()?.into_iter().filter_map(|item| -> Option<Self> {
                 Some(Self {
-                    name: item.pointer("/musicResponsiveListItemRenderer/flexColumns/0/musicResponsiveListItemFlexColumnRenderer/text/runs/0/text")?.as_str()?.to_string(),
-                    subs: item.pointer("/musicResponsiveListItemRenderer/flexColumns/1/musicResponsiveListItemFlexColumnRenderer/text/runs/2/text")?.as_str()?.to_string(),
-                    browse_id: item.pointer("/musicResponsiveListItemRenderer/navigationEndpoint/browseEndpoint/browseId")?.as_str()?.to_string(),
+                    name: item.pointer(SEARCHED_ARTIST_NAME)?.as_str()?.to_string(),
+                    subs: item.pointer(SEARCHED_ARTIST_SUBS)?.as_str()?.to_string(),
+                    browse_id: item.pointer(SEARCHED_ARTIST_ID)?.as_str()?.to_string(),
                 })
             }).collect(),
         )
@@ -128,6 +113,25 @@ impl ArtistSearchResult {
     
     pub async fn request(self: &Self, client: &Client) -> Result<Artist, Box<dyn Error>> {
         client.get_artist(&self.browse_id).await
+    }
+}
+
+fn value_from_json<'a>(value: &'a Value, pointer: &str) -> Result<&'a Value, ResponseParseError> {
+    value.pointer(pointer)
+    .ok_or(ResponseParseError::MissingValue(pointer.to_string()))
+}
+
+fn string_from_json(value: &Value, pointer: &str) -> Result<String, ResponseParseError>{
+    match value_from_json(value, pointer)?.as_str() {
+        Some(it) => Ok(it.to_string()),
+        None => Err(ResponseParseError::BadValue(pointer.to_string(), value.clone())),
+    }
+}
+
+fn iter_from_json<'a>(value: &'a Value, pointer: &str) -> Result<std::slice::Iter<'a, Value>, ResponseParseError> {
+    match value_from_json(value, pointer)?.as_array() {
+        Some(it) => Ok(it.iter()),
+        None => Err(ResponseParseError::BadValue(pointer.to_string(), value.clone())),
     }
 }
 
---
+++ src/parse/pointers.rs
@@ -0,0 +1,19 @@
+pub const ARTIST_NAME: &str = "/header/musicImmersiveHeaderRenderer/title/runs/0/text";
+pub const ARTIST_DESCIPTION: &str = "/header/musicImmersiveHeaderRenderer/description/runs/0/text";
+pub const ARTIST_ALBUMS: &str = "/contents/singleColumnBrowseResultsRenderer/tabs/0/tabRenderer/content/sectionListRenderer/contents/1/musicCarouselShelfRenderer/contents";
+pub const ARTIST_SINGLES: &str = "/contents/singleColumnBrowseResultsRenderer/tabs/0/tabRenderer/content/sectionListRenderer/contents/2/musicCarouselShelfRenderer/contents";
+pub const ARTIST_PRODUCT_NAME: &str = "/musicTwoRowItemRenderer/title/runs/0/text";
+pub const ARTIST_PRODUCT_ID: &str = "/musicTwoRowItemRenderer/title/runs/0/navigationEndpoint/browseEndpoint/browseId";
+pub const ARTIST_ALBUM_YEAR: &str = "/musicTwoRowItemRenderer/subtitle/runs/2/text";
+pub const ARTIST_SINGLE_YEAR: &str = "/musicTwoRowItemRenderer/subtitle/runs/0/text";
+
+pub const ALBUM_NAME: &str = "/header/musicDetailHeaderRenderer/title/runs/0/text";
+pub const ALBUM_YEAR: &str = "/header/musicDetailHeaderRenderer/subtitle/runs/4/text";
+pub const ALBUM_TRACKS: &str = "/contents/singleColumnBrowseResultsRenderer/tabs/0/tabRenderer/content/sectionListRenderer/contents/0/musicShelfRenderer/contents";
+pub const ALBUM_TRACK_NAME: &str = "/musicResponsiveListItemRenderer/flexColumns/0/musicResponsiveListItemFlexColumnRenderer/text/runs/0/text";
+pub const ALBUM_TRACK_ID: &str = "/musicResponsiveListItemRenderer/flexColumns/0/musicResponsiveListItemFlexColumnRenderer/text/runs/0/navigationEndpoint/watchEndpoint/videoId";
+
+pub const SEARCHED_ARTISTS: &str = "/contents/tabbedSearchResultsRenderer/tabs/0/tabRenderer/content/sectionListRenderer/contents/1/musicShelfRenderer/contents";
+pub const SEARCHED_ARTIST_NAME: &str = "/musicResponsiveListItemRenderer/flexColumns/0/musicResponsiveListItemFlexColumnRenderer/text/runs/0/text";
+pub const SEARCHED_ARTIST_SUBS: &str = "/musicResponsiveListItemRenderer/flexColumns/1/musicResponsiveListItemFlexColumnRenderer/text/runs/2/text";
+pub const SEARCHED_ARTIST_ID: &str = "/musicResponsiveListItemRenderer/navigationEndpoint/browseEndpoint/browseId";
